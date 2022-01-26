980b2c02b3b1ad4008530b4e393850234c0744ca
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-01-25 20:15:39 -0500

    Created View controllers

---
+++ Alpha/StuToDo_Alpha/StuToDo_Alpha/Controller/OnGoingTasksViewController.swift
@@ -0,0 +1,45 @@
+//
+//  OnGoingTasksViewController.swift
+//  StuToDo_Alpha
+//
+//  Created by Lunick Francois on 1/25/22.
+//
+
+import UIKit
+
+class OnGoingTasksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension OnGoingTasksViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+}
+
+extension OnGoingTasksViewController: UITableViewDelegate {
+    
+}
---
+++ Alpha/StuToDo_Alpha/StuToDo_Alpha/Controller/TasksHistoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TasksHistoryViewController.swift
+//  StuToDo_Alpha
+//
+//  Created by Lunick Francois on 1/25/22.
+//
+
+import UIKit
+
+class TasksHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
