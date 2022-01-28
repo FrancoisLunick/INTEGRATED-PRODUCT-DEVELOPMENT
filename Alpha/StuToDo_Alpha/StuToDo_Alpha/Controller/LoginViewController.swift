deed9ed1ac1a40ed6122ba516982702e9fa8dba9
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-01-28 16:23:14 -0500

    Update LoginViewController.swift

--- Alpha/StuToDo_Alpha/StuToDo_Alpha/Controller/LoginViewController.swift
+++ Alpha/StuToDo_Alpha/StuToDo_Alpha/Controller/LoginViewController.swift
@@ -6,20 +6,45 @@
 //
 
 import UIKit
+import Combine
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, Animations {
     
     weak var loginDelegate: LoginDelegate?
+    private let authManager = AuthManager()
+    private var subscribers = Set<AnyCancellable>()
+    
+    @Published var errorString: String = ""
+    @Published var isLoginSuccessful = false
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func loginButtonTapped(_ sender: UIButton) {
         
-        loginDelegate?.didLogin()
+        showLoadingAnimation()
+        
+        let email = "JohnDoe@gmail.com"
+        let password = "123456"
+        
+        authManager.login(with: email, password: password) { [weak self] result in
+            
+            self?.hideLoadingAnimation()
+            
+            switch result {
+            case .success:
+                self?.loginDelegate?.didLogin()
+            case .failure(let error):
+                self?.toast(loafState: .error, message: error.localizedDescription, duration: 3.0)
+            }
+        }
+        
+        
     }
     
     /*
