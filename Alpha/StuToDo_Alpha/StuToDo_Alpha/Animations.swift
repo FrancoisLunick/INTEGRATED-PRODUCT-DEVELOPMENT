5bf86e172146f35d3321f3be3b4fac9f3726b02c
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-01-28 16:23:00 -0500

    Update Animations.swift

--- Alpha/StuToDo_Alpha/StuToDo_Alpha/Animations.swift
+++ Alpha/StuToDo_Alpha/StuToDo_Alpha/Animations.swift
@@ -11,7 +11,6 @@ import MBProgressHUD
 
 protocol Animations {
     
-    
 }
 
 extension Animations where Self: UIViewController {
@@ -19,11 +18,21 @@ extension Animations where Self: UIViewController {
     func toast(loafState: Loaf.State, message: String, loafLocation: Loaf.Location = .top, duration: TimeInterval = 1.0) {
         
         DispatchQueue.main.async {
-            
             Loaf(message, state: loafState, location: loafLocation, sender: self).show(.custom(duration))
-            
         }
-        
+    }
+    
+    func showLoadingAnimation() {
+        DispatchQueue.main.async {
+            let hud = MBProgressHUD.showAdded(to: self.view, animated: true)
+            hud.backgroundColor = UIColor.init(white: 0.5, alpha: 0.3)
+        }
+    }
+    
+    func hideLoadingAnimation() {
+        DispatchQueue.main.async {
+            MBProgressHUD.hide(for: self.view, animated: true)
+        }
     }
     
 }
