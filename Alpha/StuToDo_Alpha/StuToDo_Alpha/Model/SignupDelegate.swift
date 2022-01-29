fdf6f883f054dcca1ef54be379ca6f0de3611cc1
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-01-29 01:29:02 -0500

    Create SignupDelegate.swift

---
+++ Alpha/StuToDo_Alpha/StuToDo_Alpha/Model/SignupDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  SignupDelegate.swift
+//  StuToDo_Alpha
+//
+//  Created by Lunick Francois on 1/29/22.
+//
+
+import Foundation
+
+protocol SignupDelegate: AnyObject {
+    
+    func didSignup()
+    
+}
