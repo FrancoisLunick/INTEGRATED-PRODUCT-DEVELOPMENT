eb9dde0c181aa97549c1677c752d2c604ecce5ce
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-01-29 00:21:33 -0500

    Update LoginViewController.swift

--- Alpha/StuToDo_Alpha/StuToDo_Alpha/Controller/LoginViewController.swift
+++ Alpha/StuToDo_Alpha/StuToDo_Alpha/Controller/LoginViewController.swift
@@ -10,6 +10,11 @@ import Combine
 
 class LoginViewController: UIViewController, Animations {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var requirementsLabel: UILabel!
+    
+    
     weak var loginDelegate: LoginDelegate?
     private let authManager = AuthManager()
     private var subscribers = Set<AnyCancellable>()
@@ -22,14 +27,60 @@ class LoginViewController: UIViewController, Animations {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        validateForm()
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        emailTextField.becomeFirstResponder()
+        
+    }
+    
+    private func validateForm() {
+        
+        $errorString.sink { [unowned self] (errorMessage) in
+            self.requirementsLabel.text = errorMessage
+        }.store(in: &subscribers)
+        
+        $isLoginSuccessful.sink { [unowned self] (isSuccessful) in
+            if isSuccessful {
+                self.loginDelegate?.didLogin()
+            }
+        }.store(in: &subscribers)
     }
     
     @IBAction func loginButtonTapped(_ sender: UIButton) {
         
+        guard let email = emailTextField.text,
+            !email.isEmpty,
+            let password = passwordTextField.text,
+            !password.isEmpty else {
+            errorString = "Login Incomplete"
+            return }
+        
+        errorString = ""
         showLoadingAnimation()
         
-        let email = "JohnDoe@gmail.com"
+        authManager.login(with: email, password: password) { [weak self] (result) in
+            self?.hideLoadingAnimation()
+            switch result {
+            case .success:
+                self?.isLoginSuccessful = true
+            case .failure(let error):
+                self?.errorString = error.localizedDescription
+            }
+        }
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+        
+        showLoadingAnimation()
+        
+        let email = "JaneDoe@gmail.com"
         let password = "123456"
         
         authManager.login(with: email, password: password) { [weak self] result in
@@ -44,7 +95,6 @@ class LoginViewController: UIViewController, Animations {
             }
         }
         
-        
     }
     
     /*
@@ -57,4 +107,14 @@ class LoginViewController: UIViewController, Animations {
     }
     */
 
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
 }
