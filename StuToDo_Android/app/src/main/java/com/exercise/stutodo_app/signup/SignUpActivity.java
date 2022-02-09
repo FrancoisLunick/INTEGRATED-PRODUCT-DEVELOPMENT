3872f51d5124ad1cf505df65ef3b75e0086b4c20
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-08 19:15:05 -0500

    Create SignUpActivity.java

---
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/signup/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.exercise.stutodo_app.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.exercise.stutodo_app.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
