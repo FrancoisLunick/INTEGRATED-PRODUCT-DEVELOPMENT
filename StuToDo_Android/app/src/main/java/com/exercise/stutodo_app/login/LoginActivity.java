d7079a04892e348b14ca81ebca14e42ee973f953
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-09 12:57:18 -0500

    Update LoginActivity.java

--- StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/login/LoginActivity.java
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/login/LoginActivity.java
@@ -1,16 +1,26 @@
 package com.exercise.stutodo_app.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.exercise.stutodo_app.MainActivity;
 import com.exercise.stutodo_app.R;
 import com.exercise.stutodo_app.signup.SignUpActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,10 +29,19 @@ public class LoginActivity extends AppCompatActivity {
     private MaterialButton mLoginButton;
     private MaterialButton mSignUpButton;
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
+    private String mEmail;
+    private String mPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
         mEmailEditText = findViewById(R.id.email_editText);
         mPasswordEditText = findViewById(R.id.password_editText);
@@ -36,6 +55,47 @@ public class LoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
 
                 startActivity(intent);
+            }
+        });
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mEmail = mEmailEditText.getText().toString();
+                mPassword = mPasswordEditText.getText().toString();
+
+                if (TextUtils.isEmpty(mEmail)) {
+
+                    mEmailEditText.setError("Email is required");
+
+                } else if (TextUtils.isEmpty(mPassword)) {
+
+                    mPasswordEditText.setError("Password is required");
+
+                } else {
+
+                    mFirebaseAuth.signInWithEmailAndPassword(mEmail, mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()) {
+
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+
+                            } else {
+
+                                Toast.makeText(LoginActivity.this, "Login Failed: " + task.getException(), Toast.LENGTH_SHORT).show();
+
+                                Log.d("Tag", "Login Failed: " + task.getException());
+                            }
+                        }
+                    });
+
+                }
+
             }
         });
     }
