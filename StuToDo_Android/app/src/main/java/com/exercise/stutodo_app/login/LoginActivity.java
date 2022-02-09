c4be49e4d6055de3930f5c092cab485c1bd2b5cf
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-08 19:15:16 -0500

    Update LoginActivity.java

--- StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/login/LoginActivity.java
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/login/LoginActivity.java
@@ -2,15 +2,41 @@ package com.exercise.stutodo_app.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.exercise.stutodo_app.R;
+import com.exercise.stutodo_app.signup.SignUpActivity;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText mEmailEditText;
+    private TextInputEditText mPasswordEditText;
+    private MaterialButton mLoginButton;
+    private MaterialButton mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmailEditText = findViewById(R.id.email_editText);
+        mPasswordEditText = findViewById(R.id.password_editText);
+        mLoginButton = findViewById(R.id.loginButton);
+        mSignUpButton = findViewById(R.id.signUpButton);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
