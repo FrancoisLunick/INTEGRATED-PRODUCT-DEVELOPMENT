66e45241c51d84e43665d953ae8f1080027a75d0
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-09 12:57:21 -0500

    Create FirebaseConstants.java

---
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/FirebaseConstants.java
@@ -0,0 +1,15 @@
+package com.exercise.stutodo_app;
+
+public class FirebaseConstants {
+
+    public static final String users = "users";
+    public static final String tasks = "Tasks";
+
+    public static final String AGE = "age";
+    public static final String EMAIL = "email";
+    public static final String FIRSTNAME = "firstname";
+    public static final String LASTNAME = "lastname";
+    public static final String PROFILEIMAGEURL = "profileImageUrl";
+    public static final String UID = "uid";
+    public static final String UNIVERSITY = "university";
+}
