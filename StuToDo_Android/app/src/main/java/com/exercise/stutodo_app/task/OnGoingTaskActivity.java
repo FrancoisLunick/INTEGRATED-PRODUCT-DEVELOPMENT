f64d02e153370affa02952acae686975be6acd5e
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-09 20:09:35 -0500

    Update OnGoingTaskActivity.java

--- StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/task/OnGoingTaskActivity.java
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/task/OnGoingTaskActivity.java
@@ -1,16 +1,68 @@
 package com.exercise.stutodo_app.task;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.exercise.stutodo_app.R;
+import com.exercise.stutodo_app.login.LoginActivity;
+import com.exercise.stutodo_app.signup.SignUpActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
 
 public class OnGoingTaskActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView mRecyclerView;
+    private FloatingActionButton mAddTaskButton;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
+    private FirebaseFirestore mDatabaseReference;
+    private StorageReference mStorageReference;
+
+    private String userID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_going_task);
+
+        mRecyclerView = findViewById(R.id.onGoingRecyclerview);
+        mAddTaskButton = findViewById(R.id.onGoingFloatingButton);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        userID = mFirebaseUser.getUid();
+        mDatabaseReference = FirebaseFirestore.getInstance();
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        mAddTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(OnGoingTaskActivity.this, AddTaskActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
