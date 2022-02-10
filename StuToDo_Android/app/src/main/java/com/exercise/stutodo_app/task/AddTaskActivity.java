d0b0f5e3a2e327f962c0db6ef8ba7d7c908c8736
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-09 23:08:58 -0500

    Update AddTaskActivity.java

--- StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/task/AddTaskActivity.java
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/task/AddTaskActivity.java
@@ -3,16 +3,24 @@ package com.exercise.stutodo_app.task;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
+import com.exercise.stutodo_app.FirebaseConstants;
 import com.exercise.stutodo_app.R;
+import com.exercise.stutodo_app.models.TaskModel;
+import com.exercise.stutodo_app.signup.SignUpActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -20,6 +28,7 @@ import com.google.firebase.storage.StorageReference;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Formatter;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class AddTaskActivity extends AppCompatActivity {
@@ -36,12 +45,16 @@ public class AddTaskActivity extends AppCompatActivity {
     private StorageReference mStorageReference;
 
     private String userID;
-    Date taskDueDate;
+    private Date taskDueDate;
+
+    private TaskModel task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        task = new TaskModel();
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
@@ -68,16 +81,57 @@ public class AddTaskActivity extends AppCompatActivity {
 
                 Date currentTime = Calendar.getInstance().getTime();
 
-                String taskId = UUID.randomUUID().toString();
-                Date createdAt = currentTime;
-                String taskTitle = titleEditText.getText().toString().trim();
-                String taskNotes = notesEditText.getText().toString().trim();
-                boolean isDone = false;
-                //Date taskDueDate =
-                String uid = userID;
+//                String taskId = UUID.randomUUID().toString();
+//                Date createdAt = currentTime;
+//                String taskTitle = titleEditText.getText().toString().trim();
+//                String taskNotes = notesEditText.getText().toString().trim();
+//                boolean isDone = false;
+//                //Date taskDueDate =
+//                String uid = userID;
 
+                task.setTaskID(UUID.randomUUID().toString());
+                task.setCreatedAt(currentTime);
+                task.setTitle(titleEditText.getText().toString().trim());
+                task.setNote(notesEditText.getText().toString().trim());
+                task.setIsDone(false);
+                task.setUid(userID);
 
+                if (TextUtils.isEmpty(task.getTitle())) {
 
+                    titleEditText.setError("Title is required");
+
+                } else if (TextUtils.isEmpty(task.getNote())) {
+
+                    notesEditText.setError("Task note is required");
+
+                } else {
+
+                    HashMap<String, Object> taskHashMap = new HashMap<>();
+
+                    taskHashMap.put(FirebaseConstants.TASKID, task.getTaskID());
+                    taskHashMap.put(FirebaseConstants.CREATEDAT, task.getCreatedAt());
+                    taskHashMap.put(FirebaseConstants.TITLE, task.getTitle());
+                    taskHashMap.put(FirebaseConstants.NOTE, task.getNote());
+                    taskHashMap.put(FirebaseConstants.ISDONE, task.getIsDone());
+                    taskHashMap.put(FirebaseConstants.UID, task.getUid());
+
+                    mDatabaseReference.collection(FirebaseConstants.tasks)
+                            .add(taskHashMap)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+
+                                    Intent intent = new Intent(AddTaskActivity.this, OnGoingTaskActivity.class);
+                                    startActivity(intent);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+
+                                }
+                            });
+                }
             }
         });
     }
