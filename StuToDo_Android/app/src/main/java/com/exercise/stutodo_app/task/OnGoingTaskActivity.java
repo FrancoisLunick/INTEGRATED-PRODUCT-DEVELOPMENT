bd77686de8768bce91b032f31fbd99345d71ab7c
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-13 02:42:04 -0500

    Update OnGoingTaskActivity.java

--- StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/task/OnGoingTaskActivity.java
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/task/OnGoingTaskActivity.java
@@ -1,5 +1,6 @@
 package com.exercise.stutodo_app.task;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -7,16 +8,27 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.exercise.stutodo_app.FirebaseConstants;
 import com.exercise.stutodo_app.R;
+import com.exercise.stutodo_app.adapter.TaskAdapter;
 import com.exercise.stutodo_app.login.LoginActivity;
+import com.exercise.stutodo_app.models.TaskModel;
 import com.exercise.stutodo_app.signup.SignUpActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 public class OnGoingTaskActivity extends AppCompatActivity {
 
@@ -32,6 +44,9 @@ public class OnGoingTaskActivity extends AppCompatActivity {
 
     private String userID;
 
+    private ArrayList<TaskModel> tasks;
+
+    private TaskAdapter taskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,15 +59,22 @@ public class OnGoingTaskActivity extends AppCompatActivity {
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        userID = mFirebaseUser.getUid();
-        mDatabaseReference = FirebaseFirestore.getInstance();
-
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setReverseLayout(true);
-        linearLayoutManager.setStackFromEnd(true);
+//        linearLayoutManager.setReverseLayout(true);
+//        linearLayoutManager.setStackFromEnd(true);
 
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        userID = mFirebaseUser.getUid();
+        mDatabaseReference = FirebaseFirestore.getInstance();
+
+        tasks = new ArrayList<TaskModel>();
+        taskAdapter = new TaskAdapter(OnGoingTaskActivity.this, tasks);
+
+        mRecyclerView.setAdapter(taskAdapter);
+
+        EventChangeListener();
 
         mAddTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +86,35 @@ public class OnGoingTaskActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void EventChangeListener() {
+
+        mDatabaseReference.collection(FirebaseConstants.tasks).orderBy(FirebaseConstants.TITLE, Query.Direction.ASCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+
+                        if (error != null) {
+
+                            Log.e("error", error.getMessage());
+
+                            return;
+                        }
+
+                        for (DocumentChange dc: value.getDocumentChanges()) {
+
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
+
+                                tasks.add(dc.getDocument().toObject(TaskModel.class));
+
+                            }
+
+                            taskAdapter.notifyDataSetChanged();
+
+                        }
+                    }
+                });
     }
 
     @Override
