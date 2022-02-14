bf9a39b1bec5001bfd1e01ec4f4c27217d18a101
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-13 19:54:43 -0500

    Create EditTaskActivity.java

---
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/task/EditTaskActivity.java
@@ -0,0 +1,16 @@
+package com.exercise.stutodo_app.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.exercise.stutodo_app.R;
+
+public class EditTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_task);
+    }
+}
