3b804fe04a77108ee7e9ed9edfe3dfbbabbc21d2
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-14 03:24:51 -0500

    Create ChangePasswordActivity.java

---
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/profile/ChangePasswordActivity.java
@@ -0,0 +1,16 @@
+package com.exercise.stutodo_app.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.exercise.stutodo_app.R;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
