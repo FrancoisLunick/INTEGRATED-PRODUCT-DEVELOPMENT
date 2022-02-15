ab53384fe743e9be4f5baec78e974da7f3ccd9ee
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-14 22:07:46 -0500

    Update ProfileActivity.java

--- StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/profile/ProfileActivity.java
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/profile/ProfileActivity.java
@@ -30,6 +30,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
