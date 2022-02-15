13959737ea959903f7f8a525cdbd8996e4f527d6
Lunick Francois <lunickfrancois@Lunicks-MacBook-Pro.local>
2022-02-14 20:34:56 -0500

    Update ChangeEmailAndPasswordActivity.java

--- StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/profile/ChangeEmailAndPasswordActivity.java
+++ StuToDo_Android/app/src/main/java/com/exercise/stutodo_app/profile/ChangeEmailAndPasswordActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.exercise.stutodo_app.R;
+import com.exercise.stutodo_app.models.User;
 import com.google.android.material.button.MaterialButton;
 
 public class ChangeEmailAndPasswordActivity extends AppCompatActivity {
@@ -14,13 +15,23 @@ public class ChangeEmailAndPasswordActivity extends AppCompatActivity {
     MaterialButton changeEmailButton;
     MaterialButton changePasswordButton;
 
+    private User user;
+    private String email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_email_and_password);
 
+        user = new User();
+
         changeEmailButton = findViewById(R.id.changeEmailButton);
         changePasswordButton = findViewById(R.id.changePasswordButton);
+
+        Intent intent = getIntent();
+        //user = intent.getParcelableExtra("user");
+        email = intent.getStringExtra("email");
+
 
         changeEmailButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +49,7 @@ public class ChangeEmailAndPasswordActivity extends AppCompatActivity {
 
                 Intent toChangePasswordIntent = new Intent(ChangeEmailAndPasswordActivity.this, ChangePasswordActivity.class);
 
+                toChangePasswordIntent.putExtra("email", email);
                 startActivity(toChangePasswordIntent);
             }
         });
